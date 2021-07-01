afb74b0bb3bb5231af3c801e2f8145880cf986f5
Salah-boutayeb <boutayebsalaheddin@gmail.com>
2021-07-01 09:27:57 +0100

    M2M application du suivi des stages v1.1.1

--- stageApp/settings.py
+++ stageApp/settings.py
@@ -165,5 +165,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'boutayebsalaheddine@gmail.com'
-EMAIL_HOST_PASSWORD = 'Salah@2000'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
